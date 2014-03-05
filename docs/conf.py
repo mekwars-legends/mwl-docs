6e05cb5f44178b58951672cf5207b3e6af2a9aeb
rhubarbselleven <web@olvec.net>
2014-03-05 17:22:30 +1100

    include todo support

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,7 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = []
+todo_include_todos=True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
